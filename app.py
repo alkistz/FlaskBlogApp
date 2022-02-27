a779af411c0aac5d8160ae38273b29f2675d46e4
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2022-02-27 11:26:00 +0100

    errr handling and timestamps

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 
 @app.errorhandler(404)
@@ -15,10 +18,10 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
